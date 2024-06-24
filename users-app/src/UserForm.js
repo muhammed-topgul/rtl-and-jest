2d97bb833856543121f380ba6b573a5fc8c337c4
mtopgul <muhammedtopgul.29@gmail.com>
2024-06-24 17:00:25 +0300

    Users App project created.

---
+++ users-app/src/UserForm.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+const UserForm = ({onUserAdd}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onUserAdd({name, email});
+        setName('');
+        setEmail('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name</label>
+                <input value={name} onChange={e => setName(e.target.value)}/>
+            </div>
+            <div>
+                <label>Email</label>
+                <input value={email} onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <button>Add User</button>
+        </form>
+    );
+};
+
+export default UserForm;
