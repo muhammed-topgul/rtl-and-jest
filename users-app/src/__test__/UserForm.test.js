2de4bbb84923685745c4f7beb42adf5ca4ff6ec1
mtopgul <muhammedtopgul.29@gmail.com>
2024-06-25 15:32:15 +0300

    UserForm test.

--- users-app/src/__test__/UserForm.test.js
+++ users-app/src/__test__/UserForm.test.js
@@ -60,3 +60,21 @@ test('It calls onUserAdd when the form is submitted (Querying by label)', () => 
     const emailInput = screen.getByLabelText(/email/i);
     testForm(nameInput, emailInput, mock);
 });
+
+test('Empties the two inputs when form is submitted', () => {
+    render(<UserForm onUserAdd={() => {}}/>);
+
+    const nameInput = screen.getByRole("textbox", {name: /name/i});
+    const emailInput = screen.getByRole("textbox", {name: /email/i});
+    const button = screen.getByRole("button");
+
+    user.click(nameInput);
+    user.keyboard("jane");
+    user.click(emailInput);
+    user.keyboard("jane@jane.com");
+
+    user.click(button);
+
+    expect(nameInput).toHaveValue('');
+    expect(emailInput).toHaveValue('');
+});
