06b3af16e0112b8ec0bbc433f81c620be12da753
mtopgul <muhammedtopgul.29@gmail.com>
2024-06-25 11:18:07 +0300

    UserForm test improvement.

--- users-app/src/__test__/UserForm.test.js
+++ users-app/src/__test__/UserForm.test.js
@@ -3,7 +3,7 @@ import user from "@testing-library/user-event";
 import UserForm from "../UserForm";
 
 test('It shows two inputs and a button', () => {
-    // render the component
+    // Render the component
     render(<UserForm/>);
 
     // Manipulate the component or find an element in it
@@ -13,4 +13,67 @@ test('It shows two inputs and a button', () => {
     // Assertion - make sure the component is doing what we expect it to do
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
+});
+
+test('It calls onUserAdd when the form is submitted (Querying by role)', () => {
+    // Render the component
+    const mock = jest.fn();
+    render(<UserForm onUserAdd={mock}/>)
+
+    // Find the two inputs
+    const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+    // Simulate typing in a name
+    user.click(nameInput);
+    user.keyboard("jane");
+
+    // Simulate typing in an email
+    user.click(emailInput);
+    user.keyboard("jane@jane.com");
+
+    // Find the button
+    const button = screen.getByRole("button");
+
+    // Simulate clicking the button
+    user.click(button);
+
+    // Assertion to make sure 'onUserAdd' gets called with name/email
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({
+        name: "jane",
+        email: "jane@jane.com"
+    });
+});
+
+test('It calls onUserAdd when the form is submitted (Querying by label)', () => {
+    // Render the component
+    const mock = jest.fn();
+    render(<UserForm onUserAdd={mock}/>)
+
+    // Find the two inputs
+    const nameInput = screen.getByRole("textbox", {
+        name: /name/i
+    });
+    const emailInput = screen.getByLabelText(/email/i);
+
+    // Simulate typing in a name
+    user.click(nameInput);
+    user.keyboard("jane");
+
+    // Simulate typing in an email
+    user.click(emailInput);
+    user.keyboard("jane@jane.com");
+
+    // Find the button
+    const button = screen.getByRole("button");
+
+    // Simulate clicking the button
+    user.click(button);
+
+    // Assertion to make sure 'onUserAdd' gets called with name/email
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({
+        name: "jane",
+        email: "jane@jane.com"
+    });
 });
