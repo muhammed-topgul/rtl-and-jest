1488537160a5b908bd5119e0bea035b7bb26f615
mtopgul <muhammedtopgul.29@gmail.com>
2024-06-26 16:10:17 +0300

    Flexible queries.

--- codesplain/src/__test__/RepositoriesSummary.test.js
+++ codesplain/src/__test__/RepositoriesSummary.test.js
@@ -14,3 +14,20 @@ test("Displays the primary language of the repository", () => {
 
     expect(language).toBeInTheDocument();
 });
+
+test("Displays information about the repository", () => {
+    const repository = {
+        language: "Javascript",
+        stargazers_count: 5,
+        open_issues: 1,
+        forks: 30
+    }
+    render(<RepositoriesSummary repository={repository}/>);
+
+    for (let key in repository) {
+        const value = repository[key];
+        const element = screen.getByText(new RegExp(value));
+
+        expect(element).toBeInTheDocument();
+    }
+});
